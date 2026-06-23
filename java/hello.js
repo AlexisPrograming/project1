8f743746c7de6c81531aefccd3af935cd623cc0c
AlexisPrograming <alexis97maga@gmail.com>
2026-06-22 20:27:17 -0400

    Add game interface and functionality for number guessing game

---
+++ java/hello.js
@@ -0,0 +1,15 @@
+let btnContinuar = document.getElementById("btn_continuar");
+
+let juego  = document.getElementById("juego");
+
+let resultado = document.getElementById("resultado");
+
+let inputNumber = document.getElementById("input_number");
+
+let btnEnviar  =  document.getElementById("btn_enviar");
+
+
+btnContinuar.addEventListener("click", function(){
+    juego.classList.add("visible");
+});
+
